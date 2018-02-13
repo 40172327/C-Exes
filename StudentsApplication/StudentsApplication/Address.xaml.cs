f5d75db7811a83a168e9a9900de94746be5449f4
DESKTOP-RBHBT6H\larry <40172327@live.napier.ac.uk>
2018-02-13 15:06:42 +0000

    Added practical 2

---
+++ StudentsApplication/StudentsApplication/Address.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentsApplication
+{
+    /// <summary>
+    /// Interaction logic for Address.xaml
+    /// </summary>
+    public partial class Address : Window
+    {
+       
+        MainWindow mainWindow;
+
+        public Address(MainWindow main)
+        {
+
+            mainWindow = main;
+            InitializeComponent();   
+        }
+
+        
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.lblAddress.Content = txtHouseN.Text+ " " 
+                +txtStreet.Text + ", " +txtPostCode.Text + ", " + txtCity.Text + ", " + txtCountry.Text;
+            this.Close();
+        }
+
+       
+    }
+}
---
+++ StudentsApplication/StudentsApplication/Course.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentsApplication
+{
+    /// <summary>
+    /// Interaction logic for Course.xaml
+    /// </summary>
+    public partial class Course : Window
+    {
+
+        MainWindow mainWindow;
+
+        public Course(MainWindow main)
+        {
+            InitializeComponent();
+            mainWindow = main;
+            comboCourse.Items.Add("Banana");
+            comboCourse.Items.Add("Apple");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.lblCourse.Content = comboCourse.SelectedItem;
+            this.Close();
+        }
+    }
+}
