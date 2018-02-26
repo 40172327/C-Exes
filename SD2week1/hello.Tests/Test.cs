9efbfbd37906355c438a6209e4504977e1e8e37d
lorenzo <40172327@live.napier.ac.uk>
2018-02-26 08:32:28 +0000

    More exercises done

---
+++ SD2week1/hello.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace hello.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ SD2week1/hello/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace hello
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
